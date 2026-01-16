7764707b7f6224b6daecce2eb573fae0e836d25a
Michael <100490812+DebSol11@users.noreply.github.com>
2026-01-16 16:01:33 +0400

    Oh no, forgot the committing show
    In case you forget to commit,
    don't worry you are not the only one,
    chilling under the sun.
    Always remember committing is supposed to be fun.
    Eating you morning bun, watching the sun.
    These committing rhymes never get bad.
    Let's ge a bit mad, and spit some nasty lines.
    No not in the mood for this food.
    Better have an I scream, calming mean.
    Which flavor sir, chocolate would be nice.
    There you don't have any surprise.
    So ein scheiß, everyone love a good surprise.
    Wie jeder weiß.

--- js/ceasarCipher.js
+++ js/ceasarCipher.js
@@ -1,17 +1,26 @@
-function ceasarCipher(string, shiftFactor) {
-  string = string.toLowerCase();
-  let result = "";
+function ceasarCipher(str, shiftFactor) {
+  // str = str.toLowerCase();
+  let result = [];
   let charCode = 0;
+  let regExPattern = /[a-z]/i; // i = case-insensitive flag
 
-  for (i = 0; i < string.length; i++) {
-    charCode = string[i].charCodeAt() + shiftFactor;
-    // pseudocode
-    // if (charCode > Z.charCode) {
-    
-    // }
-    result += charCode.fromCharCode();
+  for (i = 0; i < str.length; i++) {
+    if (regExPattern.test(str[i]) === false) {
+      result[i] = str[i];
+    } else {
+      charCode = str[i].charCodeAt() + shiftFactor;
+      result[i] = charCode;
+      if (result[i] > 122) {
+        let overflowCounter = result[i] - 122;
+        result[i] = 97 + (overflowCounter - 1);
+      }
+      result[i] = String.fromCharCode(result[i]);
+    }
   }
-  return result.replace(charCode.fromCharCode(), " ");
+  result = result.join("");
+  return result;
 }
+// string = "K"
+// console.log(string.charCodeAt());
 
-
+module.exports = ceasarCipher;
--- js/ceasarCipher.test.js
+++ js/ceasarCipher.test.js
@@ -4,3 +4,18 @@ test("check if shifting works", () => {
   const testString = "xyz";
   expect(ceasarCipher(testString, 3)).toBe("abc");
 });
+
+test("check for 100% branch coverage", () => {
+  const testString = "abc";
+  expect(ceasarCipher(testString, 5)).toBe("fgh");
+});
+
+test("check test case prevention", () => {
+  const testString = "HeLLo";
+  expect(ceasarCipher(testString, 3)).toBe("KhOOr");
+});
+
+test("test punctuation, spaces, and other non-alphabetical characters", () => {
+  const testString = "Hello, World!";
+  expect(ceasarCipher(testString, 3)).toBe("Khoor, Zruog!");
+})
